5c3af18ef2fce8c6ad90e2481ad7b2773d09cb63
Gabriel Inzurriaga <gabeinzurriaga@yahoo.com>
2019-05-16 08:39:05 -0600

    style header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { toggleProjectDisplay, generateColorPalette, toggleSaveModal } from "../../action"
+import reset from "../../images/reset.png";
+import save from "../../images/save.png";
+import folder from "../../images/folder.png"
 
 export  class Header extends Component{
 
@@ -12,9 +15,15 @@ export  class Header extends Component{
                     <h1>Palette Picker</h1>
                 </div>
                 <nav>
-                    <button className="refresh" onClick={() => {this.props.generateColorPalette(colorPalette)}}></button>
-                    <button className="save" onClick={this.props.toggleSaveModal}></button>
-                    <button className="projects" onClick={this.props.toggleProjectDisplay}></button>
+                    <button className="refresh" onClick={() => {this.props.generateColorPalette(colorPalette)}}>
+                        <img src={reset}></img>
+                    </button>
+                    <button className="save" onClick={this.props.toggleSaveModal}>
+                        <img src={save}></img>
+                    </button>
+                    <button className="projects" onClick={this.props.toggleProjectDisplay}>
+                    <img src={folder}></img>
+                    </button>
                 </nav>
             </header>
         )
